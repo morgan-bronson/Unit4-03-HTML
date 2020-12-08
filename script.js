9cfa38e4984f86c2182ed0dd5524f3cab1f72119
morganbronson <morgan.bronson@ocsbstudent.ca>
2020-12-08 15:40:23 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function hidingText () {
+  const x = document.getElementById('mydiv')
+  if (x.style.display === 'none') {
+    x.style.display = 'block'
+  } else {
+    x.style.display = 'none'
+  }
+}

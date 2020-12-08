687d185541f71ec73a56a3467ea64c0faad0e7db
morganbronson <morgan.bronson@ocsbstudent.ca>
2020-12-08 15:52:05 +0000

    Event date button

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-function hidingText () {
-  const x = document.getElementById('mydiv')
-  if (x.style.display === 'none') {
-    x.style.display = 'block'
-  } else {
-    x.style.display = 'none'
-  }
+'use strict'
+
+document.getElementById("myBtn").addEventListener("click", displayDate);
+
+function displayDate() {
+  document.getElementById("demo").innerHTML = Date();
+  alert('Event Date!')
 }
